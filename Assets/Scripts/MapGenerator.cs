e3065058216c563e536dac5bc9183b4bf3e80c7a
Sjoerd van Loon <sjoerdvanloon83@gmail.com>
2021-11-15 20:10:15 +0100

    Ep10
    Flood fill algo to produce better maps

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public int Seed = 10;
+
     public Transform TilePrefab;
     public Transform ObstaclePrefab;
     public Vector2 MapSize;
     [Range(0, 1)]
     public float OutlinePercent;
+    [Range(0, 1)]
+    public float ObstaclePercent;
 
     const string HOLDER_NAME = "Generated Map";
 
     List<Coordinate> _allTileCoordinates;
     Queue<Coordinate> _shuffledCoordinates;
+    Coordinate _mapCentre;
 
     void Start()
     {
@@ -33,6 +37,9 @@ public class MapGenerator : MonoBehaviour
         }
 
         _shuffledCoordinates = new Queue<Coordinate>(Utility.Shuffle(_allTileCoordinates.ToArray(), Seed));
+
+        // Get centre
+        _mapCentre = new Coordinate((int)MapSize.x / 2, (int)MapSize.y / 2);
 
         // Destroy group
         Transform mapHolder = transform.Find(HOLDER_NAME); // FindChild was obsolete
@@ -61,19 +68,85 @@ public class MapGenerator : MonoBehaviour
         }
 
         // Create obstacles
-        int obstacleCount = 10;
+        bool[,] obstacleMap = new bool[(int)MapSize.x, (int)MapSize.y];
+        int obstacleCount = (int)(MapSize.x * MapSize.y * ObstaclePercent); // Mapsize times obstacles percent
+        var currentObstacleCount = 0;
         for (int i = 0; i < obstacleCount; i++)
         {
-            var randomCoordinator = GetRandomCoordinate();
-            var obstaclePosition = CoordinateToPosition(randomCoordinator.x, randomCoordinator.y);
-            var newObstacle = Instantiate(ObstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity);
-            newObstacle.parent = mapHolder;//group
+            var randomCoordinate = GetRandomCoordinate();
+
+            // Prep
+            obstacleMap[randomCoordinate.x, randomCoordinate.y] = true;
+            currentObstacleCount++;
+
+            // Check if it can happend
+            if (randomCoordinate != _mapCentre && MapIsFullyAccesible(obstacleMap, currentObstacleCount))
+            {
+                // It could, so start rendering
+                var obstaclePosition = CoordinateToPosition(randomCoordinate.x, randomCoordinate.y);
+
+                var newObstacle = Instantiate(ObstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity);
+                newObstacle.parent = mapHolder; //group
+            }
+            else
+            {
+                // Could not happen, so reset
+                currentObstacleCount--;
+                obstacleMap[randomCoordinate.x, randomCoordinate.y] = false;
+            }
         }
     }
 
     private Vector3 CoordinateToPosition(int x, int y)
     {
         return new Vector3(-MapSize.x / 2 + 0.5f + x, 0, -MapSize.y / 2 + 0.5f + y); // Center each tile
+    }
+
+    bool MapIsFullyAccesible(bool[,] obstacleMap, int currentObstacleCount)
+    {
+        // Flood fill algo
+        bool[,] mapFlags = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+        Queue<Coordinate> queue = new Queue<Coordinate>();
+        queue.Enqueue(_mapCentre);
+        mapFlags[_mapCentre.x, _mapCentre.y] = true; // We know it is empty
+
+        var accessibleTileCount = 1;
+
+        while (queue.Count > 0)
+        {
+            var tile = queue.Dequeue(); // First item from the queue
+
+            // Loop through neightbour 8 tiles
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    var neighBourX = tile.x + x;
+                    var neighBourY = tile.y + y;
+
+                    var dialognally = (x == 0 || y == 0);
+                    if (dialognally)
+                    {
+                        var insideObstacleMap = (neighBourX >= 0 && neighBourX < obstacleMap.GetLength(0) && neighBourY >= 0 && neighBourY < obstacleMap.GetLength(1));
+                        if (insideObstacleMap)
+                        {
+                            var mappedFlag = mapFlags[neighBourX, neighBourY];
+                            var isObstacleTile = obstacleMap[neighBourX, neighBourY];
+
+                            if (!mappedFlag && !isObstacleTile)
+                            {
+                                mapFlags[neighBourX, neighBourY] = true; // Have checked
+                                queue.Enqueue(new Coordinate(neighBourX, neighBourY));
+                                accessibleTileCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int targetAccessibleTileCount = (int)(MapSize.x * MapSize.y - currentObstacleCount);
+
+        return targetAccessibleTileCount == accessibleTileCount;
     }
 
     public Coordinate GetRandomCoordinate()
@@ -92,5 +165,29 @@ public class MapGenerator : MonoBehaviour
             this.x = x;
             this.y = y;
         }
+
+        public static bool operator ==(Coordinate c1, Coordinate c2)
+        {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+
+
+        public static bool operator !=(Coordinate c1, Coordinate c2)
+        {
+            return c1.x != c2.x || c1.y == c2.y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Coordinate coord)
+            {
+                return this == coord;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode() => new { x, y }.GetHashCode();
+
     }
 }
